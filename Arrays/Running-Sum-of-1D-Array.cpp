4882a39a3d69e8e2dc4a0120ea09e82a3a5c71ea
Himanshi Kapoor <kapoorhimanshi617@gmail.com>
2022-09-17 20:31:48 +0530

    Added Running-Sum-of_1D-Array.cpp

--- Arrays/Running-Sum-of-1D-Array.cpp
+++ Arrays/Running-Sum-of-1D-Array.cpp
@@ -5,12 +5,8 @@ using namespace std;
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> runningSum(nums.size());
-        runningSum[0] = nums[0];
-        
         for(int i = 1; i < nums.size(); i++)
-            runningSum[i] = nums[i] + runningSum[i - 1];
-        
-        return runningSum;
+            nums[i] += nums[i - 1];
+        return nums;
     }
 };

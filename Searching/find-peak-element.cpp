3add03d90e23d444c1dfb3c2a6c37fe0ab23746f
Himanshi Kapoor <kapoorhimanshi617@gmail.com>
2023-05-06 02:02:33 +0530

    Added find-peak-element.cpp

---
+++ Searching/find-peak-element.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(nums.size() == 1)
+            return 0;
+        if(nums[0] > nums[1])
+            return 0;
+        if(nums[n - 1] > nums[n - 2])
+            return n - 1;
+        int l = 1, h = n - 2, mid = -1;
+        
+        while(l <= h) {
+            mid = l + (h - l) / 2;
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if(nums[mid] < nums[mid - 1])
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+
+        return -1;
+    }
+};

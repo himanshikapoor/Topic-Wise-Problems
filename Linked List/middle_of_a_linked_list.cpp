6678edf0c322408a257ff4b54673492ff5216851
Himanshi Kapoor <kapoorhimanshi617@gmail.com>
2025-07-31 00:30:17 +0530

    Added middle_of_a_linked_list.cpp

---
+++ Linked List/middle_of_a_linked_list.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        // fast = null when no. of nodes are even
+        // fast->next = null when no. of nodes are odd
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
